5f52f20f6698d3a78deb7d44c52429bc091f03ce
Paulo Henrique <gladgamer01@gmail.com>
2018-05-30 16:33:39 -0300

    Create Ext.cs

---
+++ Ext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeagueManager.Utils
+{
+   public class Team
+    {
+        public string name { get; set; }
+        public int wins { get; set; }
+        public int loses { get; set; }
+        public int rank { get; set; }
+    }
+}
